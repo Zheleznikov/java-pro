ac91f72325351f63262bf6e9ca15e9c23d3d3f45
Serg Zhe <polwen@yandex.ru>
2022-12-31 17:31:39 +0300

    Добавил основную логику

---
+++ HW-09JDBC/homework/src/main/java/ru/otus/HomeWork.java
@@ -0,0 +1,91 @@
+package ru.otus;
+
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.core.repository.executor.DbExecutorImpl;
+import ru.otus.core.sessionmanager.TransactionRunnerJdbc;
+import ru.otus.crm.datasource.DriverManagerDataSource;
+import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Manager;
+import ru.otus.crm.service.DbServiceClientImpl;
+import ru.otus.crm.service.DbServiceManagerImpl;
+import ru.otus.jdbc.mapper.*;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+public class HomeWork {
+    private static final String URL = "jdbc:postgresql://localhost:5430/demoDB";
+    private static final String USER = "usr";
+    private static final String PASSWORD = "pwd";
+
+    private static final Logger log = LoggerFactory.getLogger(HomeWork.class);
+
+    public static void main(String[] args) {
+// Общая часть
+        var dataSource = new DriverManagerDataSource(URL, USER, PASSWORD);
+        flywayMigrations(dataSource);
+        var transactionRunner = new TransactionRunnerJdbc(dataSource);
+        var dbExecutor = new DbExecutorImpl();
+
+// Работа с клиентом
+        EntityClassMetaData<Client> entityClassMetaDataClient = new EntityClassMetaDataImpl<>(Client.class);
+        EntitySQLMetaData entitySQLMetaDataClient = new EntitySQLMetaDataImpl(entityClassMetaDataClient);
+        var dataTemplateClient = new DataTemplateJdbc<Client>(dbExecutor, entitySQLMetaDataClient, entityClassMetaDataClient); //реализация DataTemplate, универсальная
+
+// Код дальше должен остаться
+        var dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient);
+        dbServiceClient.saveClient(new Client("dbServiceFirst"));
+
+        var clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"));
+        var clientSecondSelected = dbServiceClient.getClient(clientSecond.getId())
+                .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
+        log.info("clientSecondSelected:{}", clientSecondSelected);
+
+        // добавил проверку, что findAll и update работают
+        List<Client> clientsBeforeUpdating = dbServiceClient.findAll();
+        log.info("clientsBeforeUpdating :{}", clientsBeforeUpdating);
+
+        dbServiceClient.saveClient(new Client(clientsBeforeUpdating.get(0).getId(), "UpdatedClient"));
+        List<Client> clientsAfterUpdating = dbServiceClient.findAll();
+
+        log.info("clientsAfterUpdating:{}", clientsAfterUpdating);
+
+// Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
+
+        EntityClassMetaData entityClassMetaDataManager = new EntityClassMetaDataImpl(Manager.class);
+        EntitySQLMetaData entitySQLMetaDataManager = new EntitySQLMetaDataImpl(entityClassMetaDataManager);
+        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager, entityClassMetaDataManager);
+
+        var dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager);
+        dbServiceManager.saveManager(new Manager("ManagerFirst"));
+
+        var managerSecond = dbServiceManager.saveManager(new Manager("ManagerSecond"));
+        var managerSecondSelected = dbServiceManager.getManager(managerSecond.getNo())
+                .orElseThrow(() -> new RuntimeException("Manager not found, id:" + managerSecond.getNo()));
+        log.info("managerSecondSelected:{}", managerSecondSelected);
+
+        // добавил проверку, что findAll и update работают
+        List<Manager> managersBeforeUpdating = dbServiceManager.findAll();
+        log.info("managersBeforeUpdating :{}", managersBeforeUpdating);
+
+        dbServiceManager.saveManager(new Manager(managersBeforeUpdating.get(0).getNo(), "UpdatedManager", "updated Param2"));
+        List<Manager> managersAfterUpdating = dbServiceManager.findAll();
+
+        log.info("managersAfterUpdating:{}", managersAfterUpdating);
+
+    }
+
+    private static void flywayMigrations(DataSource dataSource) {
+        log.info("db migration started...");
+        var flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:/db/migration")
+                .load();
+        flyway.migrate();
+        log.info("db migration finished.");
+        log.info("***");
+    }
+}
---
+++ HW-09JDBC/homework/src/main/java/ru/otus/jdbc/mapper/EntitySQLMetaDataImpl.java
@@ -0,0 +1,78 @@
+package ru.otus.jdbc.mapper;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EntitySQLMetaDataImpl implements EntitySQLMetaData {
+
+    private final String tableName;
+    private final List<Field> fieldsWithoutId;
+    private final String idField;
+
+    private final String selectAllSql;
+    private final String selectByIdSql;
+    private final String insertSql;
+    private final String updateSql;
+
+
+    public EntitySQLMetaDataImpl(EntityClassMetaData entityClassMetaData) {
+
+        this.fieldsWithoutId = entityClassMetaData.getFieldsWithoutId();
+        this.idField = entityClassMetaData.getIdField().getName();
+        this.tableName = entityClassMetaData.getName().toLowerCase();
+
+        this.selectAllSql = createSelectAllSql();
+        this.selectByIdSql = createSelectByIdSql();
+        this.insertSql = createInsertSql();
+        this.updateSql = createUpdateSql();
+        System.out.println(updateSql);
+    }
+
+    private String createSelectAllSql() {
+        return "SELECT * FROM " + tableName;
+    }
+
+    private String createSelectByIdSql() {
+        return "SELECT * FROM " + tableName + " WHERE " + idField + " = ?";
+    }
+
+    private String createInsertSql() {
+        String columns = fieldsWithoutId.stream().map(Field::getName).collect(Collectors.joining(", "));
+        String numberOfQuestions = createQuestions();
+
+        return "INSERT INTO " + tableName + "(" + columns + ")" + " VALUES (" + numberOfQuestions + ")";
+    }
+
+    private String createQuestions() {
+        String questions = "?, ".repeat(fieldsWithoutId.size());
+        return questions.substring(0, questions.length() - 2);
+    }
+
+    private String createUpdateSql() {
+        String params = fieldsWithoutId.stream().map(Field::getName).collect(Collectors.joining(" = ?, ")) + " = ? ";
+        return "UPDATE " + tableName + " SET " + params + " WHERE " + idField + " = ?";
+    }
+
+    @Override
+    public String getSelectAllSql() {
+        return selectAllSql;
+    }
+
+    @Override
+    public String getSelectByIdSql() {
+        return selectByIdSql;
+    }
+
+    @Override
+    public String getInsertSql() {
+        return insertSql;
+    }
+
+    @Override
+    public String getUpdateSql() {
+        return updateSql;
+    }
+
+
+}
