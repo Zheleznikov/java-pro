94bc084852319139ff3612951c29bbc2f7947f15
Serg Zhe <polwen@yandex.ru>
2022-12-31 16:44:58 +0300

    Добавил основную логику обработки классов Client и Manager в запись в БД и обратно

--- L18-jdbc/homework/src/main/java/ru/otus/HomeWork.java
+++ L18-jdbc/homework/src/main/java/ru/otus/HomeWork.java
@@ -10,11 +10,11 @@ import ru.otus.crm.model.Client;
 import ru.otus.crm.model.Manager;
 import ru.otus.crm.service.DbServiceClientImpl;
 import ru.otus.crm.service.DbServiceManagerImpl;
-import ru.otus.jdbc.mapper.EntityClassMetaData;
-import ru.otus.jdbc.mapper.EntitySQLMetaData;
-import ru.otus.jdbc.mapper.DataTemplateJdbc;
+import ru.otus.jdbc.mapper.*;
 
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
 
 public class HomeWork {
     private static final String URL = "jdbc:postgresql://localhost:5430/demoDB";
@@ -31,9 +31,9 @@ public class HomeWork {
         var dbExecutor = new DbExecutorImpl();
 
 // Работа с клиентом
-        EntityClassMetaData entityClassMetaDataClient; // = new EntityClassMetaDataImpl();
-        EntitySQLMetaData entitySQLMetaDataClient = null; //= new EntitySQLMetaDataImpl(entityClassMetaDataClient);
-        var dataTemplateClient = new DataTemplateJdbc<Client>(dbExecutor, entitySQLMetaDataClient); //реализация DataTemplate, универсальная
+        EntityClassMetaData<Client> entityClassMetaDataClient = new EntityClassMetaDataImpl<>(Client.class);
+        EntitySQLMetaData entitySQLMetaDataClient = new EntitySQLMetaDataImpl(entityClassMetaDataClient);
+        var dataTemplateClient = new DataTemplateJdbc<Client>(dbExecutor, entitySQLMetaDataClient, entityClassMetaDataClient); //реализация DataTemplate, универсальная
 
 // Код дальше должен остаться
         var dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient);
@@ -44,11 +44,20 @@ public class HomeWork {
                 .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
         log.info("clientSecondSelected:{}", clientSecondSelected);
 
+        // добавил проверку, что findAll и update работают
+        List<Client> clientsBeforeUpdating = dbServiceClient.findAll();
+        log.info("clientsBeforeUpdating :{}", clientsBeforeUpdating);
+
+        dbServiceClient.saveClient(new Client(clientsBeforeUpdating.get(0).getId(), "UpdatedClient"));
+        List<Client> clientsAfterUpdating = dbServiceClient.findAll();
+
+        log.info("clientsAfterUpdating:{}", clientsAfterUpdating);
+
 // Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
 
-        EntityClassMetaData entityClassMetaDataManager; // = new EntityClassMetaDataImpl();
-        EntitySQLMetaData entitySQLMetaDataManager = null; //= new EntitySQLMetaDataImpl(entityClassMetaDataManager);
-        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager);
+        EntityClassMetaData entityClassMetaDataManager = new EntityClassMetaDataImpl(Manager.class);
+        EntitySQLMetaData entitySQLMetaDataManager = new EntitySQLMetaDataImpl(entityClassMetaDataManager);
+        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager, entityClassMetaDataManager);
 
         var dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager);
         dbServiceManager.saveManager(new Manager("ManagerFirst"));
@@ -57,6 +66,16 @@ public class HomeWork {
         var managerSecondSelected = dbServiceManager.getManager(managerSecond.getNo())
                 .orElseThrow(() -> new RuntimeException("Manager not found, id:" + managerSecond.getNo()));
         log.info("managerSecondSelected:{}", managerSecondSelected);
+
+        // добавил проверку, что findAll и update работают
+        List<Manager> managersBeforeUpdating = dbServiceManager.findAll();
+        log.info("managersBeforeUpdating :{}", managersBeforeUpdating);
+
+        dbServiceManager.saveManager(new Manager(managersBeforeUpdating.get(0).getNo(), "UpdatedManager", "updated Param2"));
+        List<Manager> managersAfterUpdating = dbServiceManager.findAll();
+
+        log.info("managersAfterUpdating:{}", managersAfterUpdating);
+
     }
 
     private static void flywayMigrations(DataSource dataSource) {
